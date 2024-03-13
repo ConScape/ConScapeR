a6bd79dc777f30105781c7d295b2c7245e0659ce
ninbrm <bram.van.moorter@nina.no>
2024-03-13 07:04:05 +0100

    Update NAMESPACE with devtools::document() to export functions

--- R/gridrsp.R
+++ R/gridrsp.R
@@ -1,18 +1,53 @@
+#' Title
+#'
+#' @param g
+#' @param theta
+#'
+#' @return
+#' @export
+#'
+#' @examples
 GridRSP <- function(g, theta) {
   h <- JuliaConnectoR::juliaLet("ConScape.GridRSP(g, θ=theta)", g=g, theta=theta)
   return(h)
 }
 
+#' Title
+#'
+#' @param h
+#'
+#' @return
+#' @export
+#'
+#' @examples
 betweenness_qweighted <- function(h) {
   betw <- JuliaConnectoR::juliaLet("ConScape.betweenness_qweighted(h)", h=h)
   return(betw)
 }
 
+#' Title
+#'
+#' @param h
+#' @param alpha
+#'
+#' @return
+#' @export
+#'
+#' @examples
 betweenness_kweighted <- function(h, alpha) {
   betw <- JuliaConnectoR::juliaLet("ConScape.betweenness_kweighted(h, distance_transformation=x -> exp(-x*alpha))", h=h, alpha=alpha)
   return(betw)
 }
 
+#' Title
+#'
+#' @param h
+#' @param alpha
+#'
+#' @return
+#' @export
+#'
+#' @examples
 connected_habitat <- function(h, alpha) {
   func <- JuliaConnectoR::juliaLet("ConScape.connected_habitat(h, distance_transformation=x -> exp(-x*alpha))", h=h, alpha=alpha)
   return(func)
