97cddf94053283a46e3e10ae1d5e37afe44d8aef
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2024-03-19 12:55:39 +0100

    conscape_setup: documentation

--- R/conscaper_setup.R
+++ R/conscaper_setup.R
@@ -1,11 +1,18 @@
 #' Launch the Julia installation from R and load the `ConScape` library in Julia
 #'
-#' The first time the function is ran, it is best to set the `install_libraries` to `TRUE` to install the `ConScape` library in Julia
+#' This function starts a Julia session from R and imports the `ConScape` library
+#' to be used from R. It assumes that Julia is already installed in the system
+#' and the path to its executables is given as the `julia_path` argument.
+#' The first time the function is ran, it is best to set `install_libraries = TRUE`
+#' to install the `ConScape` library in Julia.
 #'
-#' @param julia_path `[character]` The directory for the Julia bin, e.g. "C:/Programs/Julia-1.9.3/bin".
-#' @param install_libraries `[logical]` If `FALSE` (default), Julia will be launched and the required libraries loaded without installing them. If `TRUE`, the libraries will be (re-)installed in Julia.
+#' @param julia_path `[character]` \cr The directory for the Julia bin, e.g.
+#' "C:/Programs/Julia-1.9.3/bin".
+#' @param install_libraries `[logical]` \cr If `FALSE` (default), Julia will be
+#' launched and the required libraries will be loaded without installing them.
+#' If `TRUE`, the libraries will be (re-)installed in Julia.
 #'
-#' @return
+#' @return NULL.
 #' @export
 #'
 #' @example examples/ConScapeR_setup_example.R
